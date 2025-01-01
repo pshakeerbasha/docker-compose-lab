775e58b92cfbef15c97c1799c67e0bbf0a11b68e
pshakeerbasha <pshakeerbasha43@gmail.com>
2025-01-01 15:16:06 +0530

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,16 +1,17 @@
-#!/usr/bin/env python3
+import mysql.connector
+import os
 
-# Import of python system libraries.
-# These libraries will be used to create the web server.
-# You don't have to install anything special, these libraries are installed with Python.
-import http.server
-import socketserver
-
-# This variable is going to handle the requests of our client on the server.
-handler = http.server.SimpleHTTPRequestHandler
-
-# Here we define that we want to start the server on port 1234. 
-# Try to remember this information it will be very useful to us later with docker-compose.
-with socketserver.TCPServer(("", 1234), handler) as httpd:
-    # This instruction will keep the server running, waiting for requests from the client.
-    httpd.serve_forever()
+# Get MySQL host from environment variable
+mysql_host = os.getenv('MYSQL_HOST', 'localhost')  # Default to 'localhost' if the variable is not set
+connection = mysql.connector.connect(
+    host=mysql_host,
+    user='root',
+    password='rootpassword',
+    database='my_database'
+)
+cursor = connection.cursor()
+cursor.execute("SELECT DATABASE()")
+result = cursor.fetchone()
+print(f"Connected to database: {result}")
+cursor.close()
+connection.close()
